798833f784a941cc4a9eb25c68434dfa6915921e
Janik von Rotz <login@janikvonrotz.ch>
2021-06-10 13:19:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: HR
+
+Collection of hr model related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-hr.git ./addons/hr
+```
+
+## Available modules
+
+| Module                                                                | Summary                                      |
+| --------------------------------------------------------------------- | -------------------------------------------- |
+| [hr_holidays_calendar_department](hr_holidays_calendar_department/) | Adds department field and filters to report. |
---
+++ hr_holidays_calendar_department/report/hr_leave_report_calendar.py
@@ -0,0 +1,29 @@
+from odoo import fields, models, tools
+
+class LeaveReportCalendar(models.Model):
+    _inherit = "hr.leave.report.calendar"
+
+    department_id = fields.Many2one('hr.department', readonly=True)
+
+    def init(self):
+        tools.drop_view_if_exists(self._cr, 'hr_leave_report_calendar')
+
+        self._cr.execute("""CREATE OR REPLACE VIEW hr_leave_report_calendar AS
+        (SELECT 
+            row_number() OVER() AS id,
+            ce.name AS name,
+            ce.start_datetime AS start_datetime,
+            ce.stop_datetime AS stop_datetime,
+            ce.event_tz AS tz,
+            ce.duration AS duration,
+            hl.employee_id AS employee_id,
+            em.company_id AS company_id,
+            em.department_id AS department_id
+        FROM hr_leave hl
+            LEFT JOIN calendar_event ce
+                ON ce.id = hl.meeting_id
+            LEFT JOIN hr_employee em
+                ON em.id = hl.employee_id
+        WHERE 
+            hl.state = 'validate');
+        """)

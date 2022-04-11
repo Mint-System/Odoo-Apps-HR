f851dde2a472d0eb4e233b174ee52198d467e795
janikvonrotz <contact@janikvonrotz.ch>
2022-04-11 14:26:48 +0200

    update readmes

--- hr_holidays_ics_attachment/__manifest__.py
+++ hr_holidays_ics_attachment/__manifest__.py
@@ -1,20 +1,16 @@
 {
-    'name': "HR Holidays .ics Attachment",
-
-    'summary': """
+    "name": "HR Holidays .ics Attachment",
+    "summary": """
         Attach .ics file to leave approval message.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Human Resources',
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['hr_holidays'],
-
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_holidays"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
     "images": ["images/screen.png"],
 }

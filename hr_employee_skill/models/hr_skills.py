5531dc5196cf07a73886c1817014071383476267
janikvonrotz <contact@janikvonrotz.ch>
2022-05-17 16:07:24 +0200

    update skill app

--- hr_employee_skill/models/hr_skills.py
+++ hr_employee_skill/models/hr_skills.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models, _
 class EmployeeSkill(models.Model):
     _inherit = 'hr.employee.skill'
 
-    level_progress = fields.Integer(related='skill_level_id.level_progress',store=True)
+    level_progress = fields.Integer(related='skill_level_id.level_progress',store=True,group_operator="avg")

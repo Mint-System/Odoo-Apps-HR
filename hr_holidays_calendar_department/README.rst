4056d4736b10f759c872fd97161b311044767f47
janikvonrotz <contact@janikvonrotz.ch>
2022-05-30 10:58:14 +0200

    lint

--- hr_holidays_calendar_department/README.rst
+++ hr_holidays_calendar_department/README.rst
@@ -8,10 +8,17 @@ HR Holidays Calendar Department
 
 Adds department field and filters to report.
 
-.. image:: ./static/description/icon.png
+For a detailed documentation have a look at https://www.odoo-wiki.org/.
+
+.. image:: https://raw.githubusercontent.com/Mint-System/Wiki/master/assets/icon-box.png
   :height: 100
   :width: 100
   :alt: Icon
+
+Configuration
+~~~~~~~~~~~~~
+
+* No additional configurations needed
 
 Usage
 ~~~~~
@@ -22,17 +29,11 @@ Reinit module by calling the init method.
 
     env["hr.leave.report.calendar"].init()
 
-
-Configuration
-~~~~~~~~~~~~~
-
-* No additional configurations needed
-
 Maintainer
 ==========
 
-.. image:: https://www.mint-system.ch/theme_mint_system/static/img/logo.svg
-   :target: https://www.mint-system.ch
+.. image:: https://raw.githubusercontent.com/Mint-System/Wiki/master/assets/mint-system-logo.png
+  :target: https://www.mint-system.ch
 
 This module is maintained by Mint System GmbH.
 
--- hr_shortname/__manifest__.py
+++ hr_shortname/__manifest__.py
@@ -1,23 +1,16 @@
 {
-    'name': "HR Shortname",
-
-    'summary': """
+    "name": "HR Shortname",
+    "summary": """
         Set public shortname foreach employee.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Human Resources',
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['hr'],
-
-    'data': [
-        'views/hr_employee_views.xml',
-        'views/hr_employee_public_views.xml',
-    ],
-
-    'installable': True,
-    'application': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": ["views/hr_employee_views.xml", "views/hr_employee_public_views.xml"],
+    "installable": True,
+    "application": False,
+    "images": ["images/screen.png"],
 }

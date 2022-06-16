2af50fb3fe15cd0125c56cdf815a0038ebdec689
janikvonrotz <contact@janikvonrotz.ch>
2022-06-16 19:20:45 +0200

    [MIG][hr_holidays_calendar_department] migrate

---
+++ hr_holidays_calendar_department/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "HR Holidays Calendar Department",
+    "summary": """
+        Adds department field and filters to report.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_holidays"],
+    "data": ["report/hr_leave_report_calendar_views.xml", "views/show_department.xml"],
+    "installable": True,
+    "application": False,
+    "images": ["images/screen.png"],
+}
---
+++ hr_holidays_calendar_department/report/hr_leave_report_calendar.py
@@ -0,0 +1,36 @@
+from odoo import fields, models, tools
+
+class LeaveReportCalendar(models.Model):
+    _inherit = "hr.leave.report.calendar"
+
+    department_id = fields.Many2one('hr.department', readonly=True)
+    holiday_status_id = fields.Many2one('hr.leave.type', readonly=True)
+
+    def init(self):
+        tools.drop_view_if_exists(self._cr, 'hr_leave_report_calendar')
+        self._cr.execute("""CREATE OR REPLACE VIEW hr_leave_report_calendar AS
+        (SELECT 
+            row_number() OVER() AS id,
+            ce.name AS name,
+            hl.date_from AS start_datetime,
+            hl.date_to AS stop_datetime,
+            hl.employee_id AS employee_id,
+            hl.state AS state,
+            em.department_id AS department_id,
+            hl.holiday_status_id AS holiday_status_id,
+            em.company_id AS company_id,
+            CASE
+                WHEN hl.holiday_type = 'employee' THEN rr.tz
+                ELSE %s
+            END AS tz
+        FROM hr_leave hl
+            LEFT JOIN calendar_event ce
+                ON ce.id = hl.meeting_id
+            LEFT JOIN hr_employee em
+                ON em.id = hl.employee_id
+            LEFT JOIN resource_resource rr
+                ON rr.id = em.resource_id
+        WHERE 
+            hl.state IN ('confirm', 'validate', 'validate1')
+        ORDER BY id);
+        """, [self.env.company.resource_calendar_id.tz or self.env.user.tz or 'UTC'])

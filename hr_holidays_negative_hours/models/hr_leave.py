962e157c5ea434c04c658b8b67d2e017b87aaf66
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-04 16:39:07 +0100

    fix: remove loggers

--- hr_holidays_negative_hours/models/hr_leave.py
+++ hr_holidays_negative_hours/models/hr_leave.py
@@ -11,26 +11,23 @@ class HrLeave(models.Model):
         except ValidationError:
             return True
 
-    def action_draft(self):
-        try:
-            super().action_draft()
-            _logger.warning("no except")
-        except ValidationError:
-            _logger.warning("with except")
-            overtime_leaves = self.filtered("overtime_deductible")
-            overtime_leaves.overtime_id.sudo().unlink()
-            for leave in overtime_leaves:
-                overtime = (
-                    self.env["hr.attendance.overtime"]
-                    .sudo()
-                    .create(
-                        {
-                            "employee_id": leave.employee_id.id,
-                            "date": leave.date_from,
-                            "adjustment": True,
-                            "duration": -1 * leave.number_of_hours_display,
-                        }
+        def action_draft(self):
+            try:
+                super().action_draft()
+            except ValidationError:
+                overtime_leaves = self.filtered("overtime_deductible")
+                overtime_leaves.overtime_id.sudo().unlink()
+                for leave in overtime_leaves:
+                    overtime = (
+                        self.env["hr.attendance.overtime"]
+                        .sudo()
+                        .create(
+                            {
+                                "employee_id": leave.employee_id.id,
+                                "date": leave.date_from,
+                                "adjustment": True,
+                                "duration": -1 * leave.number_of_hours_display,
+                            }
+                        )
                     )
-                )
-                leave.sudo().overtime_id = overtime.id
-
+                    leave.sudo().overtime_id = overtime.id

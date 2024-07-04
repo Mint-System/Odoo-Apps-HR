5daee2fc6b9f10494655839c6abeae46a3413ff3
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-04 17:00:50 +0200

    feat: init module

---
+++ hr_attendance_user_acl/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "HR Attendance User Acl",
+    "summary": """
+          Allow attendance users to create and edit their attendance entries.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_attendance"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_attendance.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}

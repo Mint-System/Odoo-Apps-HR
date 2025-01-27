a8b66884a2092a93b4d4b158a1fa430f62d79fee
Janik von Rotz <login@janikvonrotz.ch>
2025-01-27 17:03:45 +0100

    feat: rename restricted to restrict

--- hr_employee_user_acl/__manifest__.py
+++ hr_employee_user_acl/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "HR Employee User ACL",
     "summary": """
-        Restricted access to employees app.
+        Restrict access to employees app.
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",

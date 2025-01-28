9a67d3f035a52523a24943fbfe68a2d803e5589e
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:40:05 +0100

    chore: update author

--- hr_employee_skill/__manifest__.py
+++ hr_employee_skill/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Views to manage HR employee skills.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Human Resources",
     "version": "18.0.1.0.0",

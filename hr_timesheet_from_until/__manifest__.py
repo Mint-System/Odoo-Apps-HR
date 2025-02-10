101685364e2d76ac85dbeba168eb4c4679b1a1f0
Ulrich Kilian <uk@science-and-more.de>
2025-02-10 11:55:22 +0100

    feat: migrate hr_timesheet_from_until

---
+++ hr_timesheet_from_until/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HR Timesheet From Until",
+    "summary": """
+        Calculate duration from start and end time fields.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "16.0.1.2.1",
+    "license": "AGPL-3",
+    "depends": ["hr_timesheet"],
+    "data": ["views/hr_timesheet.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}

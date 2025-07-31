25db856ef4e2746ba6f43743ed0c2d2b39cb9b0d
Ulrich Kilian <uk@science-and-more.de>
2025-07-31 14:30:21 +0200

    feat(hr_employee_paid_out_overtime_report): migrate

---
+++ hr_employee_paid_out_overtime_report/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Hr Employee Paid Out Overtime Report",
+    "summary": """
+        Shows List of employees with overtime to be paid out.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch",
+    "category": "Repository",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_attendance"],
+    "data": ["views/hr_attendance_overtime_views.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}

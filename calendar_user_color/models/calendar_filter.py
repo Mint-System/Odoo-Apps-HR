440c4438d06c607090ea08c47e0782593a0b232f
Ulrich Kilian <uk@science-and-more.de>
2025-09-01 18:27:37 +0200

    feat(calender_user_color): DRAFT state

---
+++ calendar_user_color/models/calendar_filter.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+
+class Contacts(models.Model):
+    _inherit = 'calendar.filters'
+
+    partner_color = fields.Integer(
+        related="partner_id.color",
+        store=True,
+        readonly=True,
+        default=5
+    )

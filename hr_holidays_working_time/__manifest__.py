2d05f0b3b494ed6d450e312753d52da89aa5601a
Janik von Rotz <login@janikvonrotz.ch>
2025-10-15 15:04:37 +0200

    [MIG] hr_holidays_working_time: Migration to 18.0

--- hr_holidays_working_time/__manifest__.py
+++ hr_holidays_working_time/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch/",
     "category": "Human Resources",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["hr_holidays_attendance", "hr_leave_custom_hour_interval"],
     "data": [

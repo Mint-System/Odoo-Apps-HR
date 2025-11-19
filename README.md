bffc8a85a98acf7b05bd3ba60cd3d31247164990
Janik von Rotz <login@janikvonrotz.ch>
2025-11-19 15:41:03 +0100

    feat(hr_holidays_negative_hours): show negative hours on dashboard

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ git clone git@github.com:Mint-System/Odoo-Apps-hr.git ./addons/hr
 | [hr_employee_skill](hr_employee_skill) |         Views to manage HR employee skills. |
 | [hr_employee_user_acl](hr_employee_user_acl) |         Restrict access to employees app. |
 | [hr_expense_user_acl](hr_expense_user_acl) |         Restricted access to expenses app. |
+| [hr_holidays_negative_hours](hr_holidays_negative_hours) |         Allow submit time-off request with negative extra hours. |
 | [hr_holidays_type_note](hr_holidays_type_note) |         Set note on leave type that is shown on request. |
 | [hr_holidays_user_acl](hr_holidays_user_acl) |         Restricted access to time off app. |
 | [hr_holidays_working_time](hr_holidays_working_time) |         Generate attendance entries for leaves. |
--- hr_holidays_negative_hours/models/hr_leave_type.py
+++ hr_holidays_negative_hours/models/hr_leave_type.py
@@ -14,3 +14,44 @@ class HRLeaveType(models.Model):
         help="""Once a time off of this type is approved,
         the duration will be deducted from extra hours.""",
     )
+
+    def get_allocation_data(self, employees, target_date=None):
+        """
+        Show extra hours even if they are negative.
+        """
+        res = super().get_allocation_data(employees, target_date)
+
+        # Iterate through each employee in the result
+        for employee, allocation_list in res.items():
+            extra_hours_found = any(item[0] == "Extra Hours" for item in allocation_list)
+            if not extra_hours_found:
+                extra_hours_data = (
+                    "Extra Hours",
+                    {
+                        "remaining_leaves": 0,
+                        "virtual_remaining_leaves": employee.sudo().total_overtime,
+                        "max_leaves": 0,
+                        "accrual_bonus": 0,
+                        "leaves_taken": 0,
+                        "virtual_leaves_taken": 0,
+                        "leaves_requested": 0,
+                        "leaves_approved": 0,
+                        "closest_allocation_remaining": 0,
+                        "closest_allocation_expire": False,
+                        "closest_allocation_duration": False,
+                        "holds_changes": False,
+                        "total_virtual_excess": 0,
+                        "virtual_excess_data": {},
+                        "exceeding_duration": 0,
+                        "request_unit": "hour",
+                        "icon": "/hr_holidays/static/src/img/icons/Extra_Time_Off.svg",
+                        "allows_negative": True,
+                        "max_allowed_negative": 9999,
+                        "overtime_deductible": True,
+                    },
+                    "no",
+                    -1,
+                )
+                allocation_list.append(extra_hours_data)
+
+        return res

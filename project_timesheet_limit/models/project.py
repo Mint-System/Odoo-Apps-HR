2d20cdd1be67628ec823f3232abf2b972a1c883c
Ulrich Kilian <uk@sdcience-and-more.de>
2026-02-06 18:52:36 +0100

    feat(project_timesheet_limit): add small toreance to comparison

--- project_timesheet_limit/models/project.py
+++ project_timesheet_limit/models/project.py
@@ -1,7 +1,8 @@
 import logging
 
-from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+
+from odoo import _, api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -20,6 +21,9 @@ class ProjectTask(models.Model):
     @api.onchange("allocated_hours")
     def _check_limit_timehseet_amount(self, unit_amount=0, line=None):
         """Effective hours must no exceed allocated hours."""
+
+        # small tolerance becaus of rounding issues
+        TOLERANCE = 0.01
 
         for task in self.filtered(
             lambda t: t.allocated_hours != 0.0 and t.project_id.limit_timehseet_amount
@@ -46,7 +50,7 @@ class ProjectTask(models.Model):
 
             # _logger.warning([effective_hours, planned_hours, task.subtask_planned_hours])
 
-            if effective_hours > allocated_hours:
+            if effective_hours > (allocated_hours + TOLERANCE):
                 raise UserError(
                     _(
                         "The timesheeted amount exceeds the allocated hours (%(allocated_hours)s) of the task."

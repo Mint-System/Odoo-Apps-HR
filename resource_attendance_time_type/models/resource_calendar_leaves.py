94c67ef2ddc673b72aff788e5ca4c061c9214ed5
laurens kooijman <laurenskooijman@gmail.com>
2026-06-15 07:56:13 +0200

    refactor(resource_attendance_time_type): added selection_add functionality instead of overwriting

--- resource_attendance_time_type/models/resource_calendar_leaves.py
+++ resource_attendance_time_type/models/resource_calendar_leaves.py
@@ -10,5 +10,5 @@ _logger = logging.getLogger(__name__)
 class ResourceCalendarLeaves(models.Model):
     _inherit = "resource.calendar.leaves"
 
-    time_type = fields.Selection([('leave', 'Time Off'), ('other', 'Other'), ('attendance', 'Attendance')], default='leave',
-                                 help="Whether this should be computed as a time off or as work time (eg: formation)")
+    time_type = fields.Selection(selection_add=[('attendance', 'Attendance')])
+    

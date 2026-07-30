284c96d8b136e07c979ac833047a9e407e038f19
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-30 14:48:56 +0200

    feat(fleet_hr_vehicle_request): migrate

---
+++ fleet_hr_vehicle_request/tests/TEST_INSTRUCTIONS.rst
@@ -0,0 +1,14 @@
+Return vehicle:
+
+- Go to Employees > Vehicle Request > Vehicle Request
+- Create new vehicle request
+- Send request
+- Approve request
+- Set "Last Odometer Value"
+- Click "Return" button
+
+Confirm vehicle settings:
+
+- Go to Fleet and edit vehicle you used
+- Odometer value is set to value of return
+- Driver history is updated
---
+++ hr_expense_vehicle_request/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "HR Expense Vehicle Request",
+    "summary": """
+        Create expenses from retourned vehicle requests.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "14.0.3.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_expense", "fleet_hr_vehicle_request"],
+    "data": [
+        "data/hr_expense.xml",
+        "views/hr_expense.xml",
+        "views/vehicle_request.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
